a896b6f6300ef9418accc2607e0a3eac6d0b203b
avi33 <avi.gazneli@gmail.com>
2023-01-04 13:25:09 +0200

    cleanup

--- train.py
+++ train.py
@@ -6,13 +6,10 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from torchvision.transforms.transforms import ToPILImage
 import yaml
-from torchvision.datasets import CIFAR10 as Dataset
 import numpy as np
 import argparse
 from pathlib import Path
 import torchvision.transforms as T
-from cifar_quant import save_torchscript_model
-from modules.modules import Net, NetQ, create_net
 from helper_funcs import add_weight_decay
 import logger
 import copy
@@ -23,8 +20,10 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--batch_size", default=64, type=int)
     parser.add_argument("--n_epochs", default=100, type=int)
+    parser.add_argument("--dataset", default="cifar10", type=str)
     '''net'''
-    parser.add_argument("--net_type", default="cnn", type=str)
+    parser.add_argument("--tf_type", default="torch", type=str)
+    parser.add_argument("--n_classes", default=10, type=int)
     '''optimizer'''
     parser.add_argument("--max_lr", default=3e-4, type=float)
     parser.add_argument("--wd", default=1e-4, type=float)
@@ -33,30 +32,46 @@ def parse_args():
     '''loss'''
     parser.add_argument("--loss_type", default="label_smooth", type=str)
     '''debug'''
-    parser.add_argument("--save_path", default='outputs/quant', type=Path)
-    parser.add_argument("--load_path", default='outputs/quant', type=Path)
+    parser.add_argument("--save_path", default='outputs/tmp', type=Path)
+    parser.add_argument("--load_path", default=None, type=Path)
     parser.add_argument("--save_interval", default=100, type=int)    
-    parser.add_argument("--log_interval", default=100, type=int)
-    '''quantization'''
-    parser.add_argument("--quant", action="store_true", default=True)
+    parser.add_argument("--log_interval", default=100, type=int)          
     
     args = parser.parse_args()
     return args
 
 def create_dataset(args):
-    train_augs = T.Compose([#T.ColorJitter(hue=.05, saturation=.05),
-                            T.RandomHorizontalFlip(p=0.5),
-                            T.RandomRotation(degrees=15),
-                            T.RandomResizedCrop(size=(32, 32), scale=(0.5, 1.0)),                            
-                            T.ToTensor(), 
+    if args.dataset == 'cifar10':
+        from torchvision.datasets import CIFAR10 as Dataset
+        train_augs = T.Compose([#T.ColorJitter(hue=.05, saturation=.05),
+                                T.RandomHorizontalFlip(p=0.5),
+                                T.RandomRotation(degrees=15),
+                                T.RandomResizedCrop(size=(32, 32), scale=(0.5, 1.0)),                            
+                                T.ToTensor(), 
+                                T.Normalize([0.5]*3, [0.5]*3)])
+        
+        test_augs = T.Compose([T.ToTensor(), 
                             T.Normalize([0.5]*3, [0.5]*3)])
+
+        train_set = Dataset(train=True, transform=train_augs, download=False, root='data')    
+        test_set = Dataset(train=False, transform=test_augs, download=False, root='data')
+
+    elif args.dataset == 'svhn':
+        from torchvision.datasets import SVHN as Dataset
+        train_augs = T.Compose([#T.ColorJitter(hue=.05, saturation=.05),
+                                T.RandomHorizontalFlip(p=0.5),
+                                T.RandomRotation(degrees=15),
+                                T.RandomResizedCrop(size=(32, 32), scale=(0.5, 1.0)),                            
+                                T.ToTensor(), 
+                                T.Normalize([0.5]*3, [0.5]*3)])
+        
+        test_augs = T.Compose([T.ToTensor(), 
+                            T.Normalize([0.5]*3, [0.5]*3)])
+
+        train_set = Dataset(train=True, transform=train_augs, download=False, root='data')    
+        test_set = Dataset(train=False, transform=test_augs, download=False, root='data')
+
     
-    test_augs = T.Compose([T.ToTensor(), 
-                           T.Normalize([0.5]*3, [0.5]*3)])
-
-    train_set = Dataset(train=True, transform=train_augs, download=False, root='data')    
-    test_set = Dataset(train=False, transform=test_augs, download=False, root='data')
-
     return train_set, test_set
 
 def train():
@@ -76,22 +91,10 @@ def train():
     train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, drop_last=True, num_workers=4, pin_memory=True)
     test_loader = DataLoader(test_set, batch_size=args.batch_size, shuffle=False, drop_last=False, num_workers=4, pin_memory=True)
     '''net'''
-    net = Net()
-    if args.quant:
-        net = NetQ(model_flp=net)
-        # fuse the activations to preceding layers, where applicable
-        # this needs to be done manually depending on the model architecture
-        # fused_model = copy.deepcopy(model)
-                
-        # net_fused = torch.quantization.fuse_modules(net, [['conv', 'bn', 'relu']])
-
-        # Prepare the model for QAT. This inserts observers and fake_quants in
-        # the model that will observe weight and activation tensors during calibration.        
-        net.qconfig = torch.quantization.get_default_qconfig("fbgemm")
-        # https://pytorch.org/docs/stable/_modules/torch/quantization/quantize.html#prepare_qat
-        torch.quantization.prepare_qat(net, inplace=True)            
-    # net.train()
+    from modules.models import Net
+    net = Net(emb_dim=128, n_classes=args.n_classes, nf=16, tf_type=args.tf_type)
     net.to(device)
+    
     '''optimizer'''
     if args.amp:
         from torch.cuda.amp import GradScaler
@@ -110,8 +113,6 @@ def train():
                                                        epochs=args.n_epochs,
                                                        pct_start=0.1,                                                       
                                                     )    
-    if args.quant:
-        args.ema = None
     if args.ema is not None:
         from ema import ModelEma as EMA
         ema = EMA(net, decay_per_epoch=args.ema)
@@ -137,7 +138,6 @@ def train():
     loss_ce = nn.CrossEntropyLoss(reduction="sum").to(device)
 
     torch.backends.cudnn.benchmark = True
-    best_acc = -1
     acc_test = 0
     steps = 0        
     skip_scheduler = False
@@ -148,7 +148,6 @@ def train():
         net.load_state_dict(checkpoint['model_dict'])
         opt.load_state_dict(checkpoint['opt_dict'])
         steps = checkpoint['resume_step'] if 'resume_step' in checkpoint.keys() else 0
-        best_acc = checkpoint['best_acc']
         print('checkpoints loaded')
 
     for epoch in range(1, args.n_epochs + 1):
@@ -169,14 +168,7 @@ def train():
             y = y.to(device)
             with torch.cuda.amp.autocast(enabled=scaler is not None):                
                 y_est = net(x)
-                if args.loss_type == 'label_smooth_dirichlet':
-                    noise = torch.zeros_like(y_est)
-                    alpha = None
-                    if start_est:
-                        alpha = D(y_est)
-                    loss = criterion(y_est, y, alpha)
-                else:
-                    loss = criterion(y_est, y)
+                loss = criterion(y_est, y)
 
             if args.amp:
                 scaler.scale(criterion).backward()
@@ -195,53 +187,42 @@ def train():
 
             if not skip_scheduler:
                 lr_scheduler.step()
-            
+
             '''metrics'''            
             acc = logger.accuracy(y_est, target=y, topk=(1,))[0]
-            if args.loss_type == 'label_smooth_dirichlet' and acc > 0:
-                start_est = True
             
             metric_logger.update(loss=loss.item())
             metric_logger.update(acc=acc)
-            metric_logger.update(lr=opt.param_groups[0]["lr"])                                
-
+            metric_logger.update(lr=opt.param_groups[0]["lr"])
             ######################
             # Update tensorboard #
-            ######################             
-            writer.add_scalar("train/loss", loss.item(), steps)
-            writer.add_scalar("train/acc1", acc, steps)
-            writer.add_scalar("lr", lr_scheduler.get_last_lr()[0], steps)            
-
+            ######################               
             steps += 1                        
-            if steps % args.save_interval == 0:
+            if steps % args.save_interval != 0:
+                writer.add_scalar(f"loss/train", loss.item(), steps)
+                writer.add_scalar(f"acc/train", acc, steps)
+                writer.add_scalar(f"lr", lr_scheduler.get_last_lr()[0], steps)
+            else:
                 acc_test = 0
                 loss_test = 0
-                net.eval()                
-                if args.quant:
-                    net.cpu()
-                    net_q = torch.quantization.convert(net)
-                    net_q.eval()
+                net.eval()
                 with torch.no_grad():                                        
-                    for i, (x, y) in enumerate(test_loader):                        
-                        if args.quant:
-                            y_est = net_q(x)
-                        else:
-                            x = x.to(device)
-                            y = y.to(device)
-                            y_est = net(x)
+                    for i, (x, y) in enumerate(test_loader):                                                
+                        x = x.to(device)
+                        y = y.to(device)
+                        y_est = net(x)
                         loss_test += loss_ce(y_est, y).item()
                         acc_test += logger.accuracy(y_est, target=y, topk=(1, ))[0]
                                 
                 loss_test /= len(test_loader)
                 acc_test /= len(test_loader)
 
-                writer.add_scalar("test/loss", loss_test, steps)
-                writer.add_scalar("test/acc1", acc_test, steps)
+                writer.add_scalar("loss/test", loss_test, steps)
+                writer.add_scalar("acc/test", acc_test, steps)
 
                 metric_logger.update(loss_test=loss_test)
                 metric_logger.update(acc_test=acc)
-                if args.quant:
-                    net.to(device)
+
                 net.train()                
                                 
                 best_acc = metric_logger.meters['acc_test'].deque[0]
@@ -251,8 +232,6 @@ def train():
                     'step': steps,
                     'best_acc': acc                        
                 }
-                if args.quant:
-                    save_torchscript_model(net_q, args.save_path, "net_q_jit.pt")
                 torch.save(chkpnt, root / "chkpnt.pt")
 
 if __name__ == "__main__":
